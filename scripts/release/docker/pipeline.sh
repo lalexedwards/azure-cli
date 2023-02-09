1cadf874f94e72028e70ddedfb5ecef0f18dde00
Hang Lei <hanglei@microsoft.com>
2023-02-09 15:52:20 +0800

    Test

--- scripts/release/docker/pipeline.sh
+++ scripts/release/docker/pipeline.sh
@@ -8,12 +8,13 @@ set -evx
 
 IMAGE_NAME=clibuild$BUILD_BUILDNUMBER
 CLI_VERSION=`cat src/azure-cli/azure/cli/__main__.py | grep __version__ | sed s/' '//g | sed s/'__version__='// |  sed s/\"//g`
+tar czf $BUILD_DIRECTORY/azure-cli.tar.gz --directory=$BUILD_SOURCESDIRECTORY .
 
 docker build --no-cache \
              --build-arg BUILD_DATE="`date -u +"%Y-%m-%dT%H:%M:%SZ"`" \
              --build-arg CLI_VERSION=$CLI_VERSION \
              --tag $IMAGE_NAME:latest \
-             --secret id=azure,src=`pwd` \
+             -e BUILD_DIRECTORY=$BUILD_BUILDNUMBER \
              $BUILD_SOURCESDIRECTORY
 
 docker save -o "$BUILD_STAGINGDIRECTORY/docker-azure-cli-${CLI_VERSION}.tar" $IMAGE_NAME:latest
